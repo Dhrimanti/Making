b5ac00af00e619408d33a02621a10d81d1178e02
Dhrimanti <dhritimancg@gmail.com>
2024-12-27 21:11:49 +0530

    PLs

--- sd/encoder.py
+++ sd/encoder.py
@@ -2,3 +2,6 @@ import torch
 from torch import nn
 from torch.nn import functional as f
 import torch
+from decoder import VAE_AttentionBlock,VAR_ResidualBlock
+class VAE_Encoder(nn.Sequential):
+    
