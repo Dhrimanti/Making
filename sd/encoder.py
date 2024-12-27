2e8e9c69f30e5e549608c7f024367748dd1ccbdd
Dhrimanti <dhritimancg@gmail.com>
2024-12-27 21:25:41 +0530

    Pls

--- sd/encoder.py
+++ sd/encoder.py
@@ -2,6 +2,19 @@ import torch
 from torch import nn
 from torch.nn import functional as f
 import torch
-from decoder import VAE_AttentionBlock,VAR_ResidualBlock
+from decoder import VAE_AttentionBlock,VAE_ResidualBlock
 class VAE_Encoder(nn.Sequential):
-    
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(3,128,kernel_size=3,padding=1),
+            VAE_ResidualBlock(128,128),
+            VAE_ResidualBlock(128,128),
+            nn.Conv2d(3,128,kernel_size=3,padding=0,stride=2),
+            VAE_ResidualBlock(128,256),
+            VAE_ResidualBlock(128,256),
+            
+            
+            
+            
+
+        )
