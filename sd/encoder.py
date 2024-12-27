b602109adc75ffcdb861b94c58d981dcf148cf3d
Dhrimanti <dhritimancg@gmail.com>
2024-12-27 22:53:37 +0530

    Hello

--- sd/encoder.py
+++ sd/encoder.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.nn import functional as f
 import torch
 from decoder import VAE_AttentionBlock,VAE_ResidualBlock
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class VAE_Encoder(nn.Sequential):
     def __init__(self):
         super().__init__(
@@ -11,10 +12,21 @@ class VAE_Encoder(nn.Sequential):
             VAE_ResidualBlock(128,128),
             nn.Conv2d(3,128,kernel_size=3,padding=0,stride=2),
             VAE_ResidualBlock(128,256),
-            VAE_ResidualBlock(128,256),
+            VAE_ResidualBlock(256,256),
+            nn.Conv2d(256,256,kernel_size=3,padding=0,stride=2),
+            VAE_ResidualBlock(256,512),
+            VAE_ResidualBlock(512,512),
+            nn.Conv2d(512,512,kernel_size=3,padding=0,stride=2),
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            VAE_AttentionBlock
+            
             
             
             
             
 
         )
+        self.to(device)
+        
