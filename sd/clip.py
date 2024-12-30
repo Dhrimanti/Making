4763d6082a8f9be7b0daa7d594d50476960bc7ea
Dhrimanti <dhritimancg@gmail.com>
2024-12-30 21:39:52 +0530

    Hello

--- sd/clip.py
+++ sd/clip.py
@@ -5,7 +5,34 @@ from attention import SelfAttention
 
 
 
+class CLIPEmbedding(nn.Module):
+    def __init__(self,vocab_size:int,n_embed:int,n_tokens:int):
+        super().__init__()
+        self.token_embedding=nn.Embedding(n_vocab,n_embed)
+        self.position_embedding=nn.Parameter(torch.zeros(n_tokens,n_embed))
+    def forward(self,tokens):
+        x=self.token_embedding(tokens)
+        x+=self.position_embedding
+        return x
 
+class CLIPLayer(nn.Module):
+    def __init__(self,n_layers:int,n_embed:int):
+        super().__init__()
+        self.layernorm_1=nn.LayerNorm(n_embed)
+        self.attention=SelfAttention(n_head,n_embed)
+        self.layernorm_2=nn.LayerNorm(n_embed)
+        self.linear_1=nn.Linear(n_embed,4*n_embed)
+    def forward(self,x:torch.Tensor)->torch.Tensor:
+        residue=x
+        x=self.layernorm_1(x)
+        x=self.attention(x,causal_mask=True)
+        x+=residue
+        residue=x
+        x=self.layernorm_2(x)
+        x=self.linear_1(x)
+        x=x*torch.sigmoid(1.702*x)
+        x+=residue
+        return x
 
 class CLIP(nn.Module):
     def __init__(self):
