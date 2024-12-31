988dc94712a820d179a7b2ad74e56ed3277937da
Dhrimanti <dhritimancg@gmail.com>
2025-01-01 00:37:29 +0530

    Hello

--- sd/ddpm.py
+++ sd/ddpm.py
@@ -15,6 +15,8 @@ class DDPMSampler:
         step_ratio=self.num_training_steps//self.num_inference_steps
         timesteps=(np.arange(0,num_inference_steps)*step_ratio).round()[::-1].copy().astype(np.int64)
         self.timesteps=torch.from_numpy(timesteps) 
+
+    def step
     def add_noise(self,original_samples:torch.FloatTensor,timesteps:torch.IntTensor)->torch.FloatTensor:
         alphas_cumprod=self.alphas_cumprod.to(device=original_samples.device,dtype=original_samples.dtype)
         timesteps=timesteps.to(original_samples.device)
@@ -26,4 +28,7 @@ class DDPMSampler:
         sqrt_one_minus_alpha_prod=sqrt_one_minus_alpha_prod.flatten()
         while len(sqrt_one_minus_alpha_prod.shape)<len(original_samples.shape):
             sqrt_alpha_prod=sqrt_alpha_prod.unsqueeze(-1)
+        noise=torch.randn(original_samples.shape,generator=self.generator,device=original_samples.device,dtype=original_samples.dtype)
+        noisy_samples=(sqrt_alpha_prod*original_samples)+(sqrt_one_minus_alpha_prod)*noise
+        return noisy_samples
          
